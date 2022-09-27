38a7ee1eae8d190346934de9d6defc6f912086ec
cruz <vcruzj15@estudiantec.cr>
2022-09-27 17:09:39 -0600

    The filters for the images are added to the main branch. Now the filters uses a pointer to apply the filters to the images.

--- Processing_APIS.cpp
+++ Processing_APIS.cpp
@@ -1,41 +1,39 @@
 //
 // Created by cruz on 19/09/22.
 //
-
+#include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include "Processing_APIS.h"
+#include <opencv2/imgcodecs.hpp>
 #include <opencv2/core.hpp>
 
 using namespace cv;
 
-Mat Processing_APIS::gaussian_blur(Mat image) {
-    Mat image_out;
-    GaussianBlur(image, image_out,Size(15,15), 0);
-    return image_out;
+int Processing_APIS::gaussian_blur(Mat* img, Mat* img2) {
+    GaussianBlur(*img, *img2,Size(15,15), 0);
+    return 0;
 }
 
-Mat Processing_APIS::gamma_correction(Mat image) {
-
-    Mat image_out;
-    int gamma = 2;
+int Processing_APIS::gamma_correction(Mat* img, Mat* img2) {
     double c, r_min,r_max;
-    minMaxLoc(image, &r_min,&r_max);
+    float gamma= 2.5;
+    minMaxLoc(*img, &r_min,&r_max);
     c= 255/pow(r_max,gamma);
-    image.convertTo(image,CV_32F);
-    pow(image, gamma, image_out);
-    image_out = c * image_out;
-    image_out.convertTo(image_out,CV_8UC1);
-    return image_out;
+    img->convertTo(*img,CV_32F);
+    pow(*img, gamma, *img2);
+    *img2= c*(*img2);
+    img2->convertTo(*img2,CV_8UC1);
+
+    return 0;
 }
 
-Mat Processing_APIS::bright_control(Mat image, int bright) {
-    Mat image_out;
-    image.convertTo(image_out,-1, 1 ,bright-150);
-    return image_out;
+int Processing_APIS::bright_control(Mat* img, Mat* img2, int bright) {
+
+    img->convertTo(*img2,-1,1,bright-150);
+    return 0;
 }
 
-Mat Processing_APIS::gray_scale(Mat image) {
-    Mat image_out;
-    cvtColor(image,image_out, COLOR_RGB2GRAY);
-    return image_out;
+int Processing_APIS::gray_scale(Mat* img, Mat* img2) {
+    cvtColor(*img,*img2, COLOR_BGR2GRAY);
+    return 0;
 }
--- Processing_APIS.h
+++ Processing_APIS.h
@@ -4,19 +4,22 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-#ifndef IMAGE_PROCESSING_PROCESSING_APIS_H
-
 using namespace cv;
 
 class Processing_APIS{
 
 public:
-    static Mat gaussian_blur(Mat image);
-    static Mat gamma_correction(Mat image);
-    static Mat bright_control(Mat image, int bright);
-    static Mat gray_scale(Mat image);
+    static int gaussian_blur(Mat* img, Mat* img2);
+    static int gamma_correction(Mat* img, Mat* img2);
+    static int bright_control(Mat* img, Mat* img2, int bright);
+    static int gray_scale(Mat* img, Mat* img2);
+
+private:
+
 };
 
+
+#ifndef IMAGE_PROCESSING_PROCESSING_APIS_H
 #define IMAGE_PROCESSING_PROCESSING_APIS_H
 
 #endif //IMAGE_PROCESSING_PROCESSING_APIS_H
